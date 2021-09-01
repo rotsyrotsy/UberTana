b7fc7ccb576b34143ce8e19cc5aa56ae9091bfad
Haky <ralaielarivony@gmail.com>
2021-09-01 23:54:58 +0300

    update base

--- ubertana.sql
+++ ubertana.sql
@@ -14,6 +14,8 @@ CREATE TABLE Passager(
 CREATE TABLE Client(
     email VARCHAR(20) NOT NULL PRIMARY KEY,
     nom VARCHAR(20),
+    modele VARCHAR(20),
+    matricule VARCHAR(20),
     mdp VARCHAR(50),
     numTel VARCHAR(30),
     nationalite VARCHAR(20),
@@ -45,4 +47,31 @@ CREATE TABLE Note(
     foreign key (emailPassager) references Passager(email) ON DELETE CASCADE
 );
 
+CREATE TABLE ClientRequest(
+    IdCR VARCHAR(20) NOT NULL PRIMARY KEY,
+    locLogDep NUMERIC(20,15),
+    locLatDep NUMERIC(20,15),
+    locLogArr NUMERIC(20,15),
+    locLatArr NUMERIC(20,15),
+    date_Time TIMESTAMP,
+    emailPassager VARCHAR(20),
+    foreign key (emailPassager) references Passager(email) ON DELETE CASCADE
+);
 
+CREATE TABLE DriverProposition(
+    IdDrivProp VARCHAR(20) NOT NULL PRIMARY KEY,
+    IdCR VARCHAR(20),
+    IdDriver VARCHAR(20),
+    propostion DOUBLE PRECISION,
+    statue VARCHAR(20),
+    foreign key (IdCR) references ClientRequest(IdCR) ON DELETE CASCADE,
+    foreign key (IdDriver) references Client(email) ON DELETE CASCADE
+);
+
+CREATE TABLE Match(
+    IdMatch VARCHAR(20) NOT NULL PRIMARY KEY,
+    IdDriver VARCHAR(20),
+    IdCR VARCHAR(20),
+    foreign key (IdCR) references ClientRequest(IdCR) ON DELETE CASCADE,
+    foreign key (IdDriver) references Client(email) ON DELETE CASCADE
+);
