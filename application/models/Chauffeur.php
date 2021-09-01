4b64a6a1f071f411d1e5fc6e4d7c1b745aecdf91
Princia08 <80781644+Princia08@users.noreply.github.com>
2021-09-01 22:48:27 +0300

    b

--- application/models/Chauffeur.php
+++ application/models/Chauffeur.php
@@ -13,5 +13,6 @@ class Model extends CI_Model
     public function payer($email,$valeur,$date_heure)
     {
         $sql = "insert into paiement values(nextval('seqPaiement'),'"+$emailClient+"',"+$valeur+",'"+$date_heure+"')";
+        $this->db->query($sql);
     } 
 }
