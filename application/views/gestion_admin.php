f914e4f45a5c7428f823b55eb5ecbc078e0cf86b
syraxxxx <rsitraka181@gmail.com>
2021-09-07 22:28:33 +0200

    gestion_admin

--- application/views/gestion_admin.php
+++ application/views/gestion_admin.php
@@ -1,3 +1,4 @@
+
 <section class="contact_section layout_padding-bottom layout_padding2-top">
     <div class="container px-0">
       <div class="heading_container">
@@ -9,16 +10,23 @@
     </div>
     <div class="container container-bg">
       <div class="row">
-        <div class="col-md-5 col-lg-4 px-0">
-            <ul>
-                <li><a href="#">Client</a></li>
-                <li><a href="#">Chauffeur</a></li>
-                <li><a href="#">Coin</a></li>
-            </ul>
+        <div class="col-md-5 col-lg-4 px-0" class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Client</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Chauffeur</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Coin</a>
+                </li>
+                </ul>
         </div>
         
         <div class="col-lg-8 col-md-7 px-0">
             <h2>Accueil gestion </h2>  
+            
         </div>
         
       </div>
