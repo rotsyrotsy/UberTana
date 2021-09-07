7148006f62892a0447360d082429b28c829ff957
syraxxxx <rsitraka181@gmail.com>
2021-09-07 20:08:10 +0200

    update accueil

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -1,24 +1,102 @@
-<section class="agency_section layout_padding-bottom">
-    <div class="agency_container ">
-      <div class="box ">
-        <div class="detail-box">
-          <div class="heading_container">
-            <h2>
-              A Propos de <span>Uber</span> Tana
-            </h2>
+<section class="slider_section">
+      <div class="slider_container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-6 px-0">
+                    <div class="img-box">
+                      <img src="<?php echo site_url("assets/images/slider-img.jpg") ?>" alt="" />
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="detail-box">
+                      <h1>
+                        Design
+                        <br />
+                        Agency
+                      </h1>
+                      <p>
+                        There are many variations of passages of Lorem Ipsum available, but the
+                      </p>
+                      <a href="">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-6 px-0">
+                    <div class="img-box">
+                      <img src="<?php echo site_url("assets/images/slider-img.jpg") ?>" alt="" />
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="detail-box">
+                      <h1>
+                        Design
+                        <br />
+                        Agency
+                      </h1>
+                      <p>
+                        There are many variations of passages of Lorem Ipsum available, but the
+                      </p>
+                      <a href="">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-6 px-0">
+                    <div class="img-box">
+                      <img src="<?php echo site_url("assets/images/slider-img.jpg") ?>" alt="" />
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="detail-box">
+                      <h1>
+                        Design
+                        <br />
+                        Agency
+                      </h1>
+                      <p>
+                        There are many variations of passages of Lorem Ipsum available, but the
+                      </p>
+                      <a href="">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          </p>
-          <a href="">
-            Voir Plus 
-          </a>
+          <div class="carousel_btn-box">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="sr-only">Previous</span>
+            </a>
+            <img src="<?php echo site_url("assets/images/line.png") ?>" alt="" />
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-
-  <section class="started_section layout_padding-bottom">
+    </section>
+<br>
+<br>
+    <section class="started_section layout_padding-bottom">
     <div class="container">
       <div class="row">
         <div class="col-md-7">
@@ -44,3 +122,24 @@
       </div>
     </div>
   </section>
+
+<section class="agency_section layout_padding-bottom">
+    <div class="agency_container ">
+      <div class="box ">
+        <div class="detail-box">
+          <div class="heading_container">
+            <h2>
+              A Propos de <span>Uber</span> Tana
+            </h2>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          </p>
+          <a href="">
+            Voir Plus 
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
